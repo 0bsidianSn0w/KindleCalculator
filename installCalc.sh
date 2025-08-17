f94ccaf9b3728be225f807be23fcb42cb9d26262
0bsidianSn0w <ThisIsNotMyEmail@gmail.iom>
2025-08-17 22:11:54 +0100

    fix file being left after install - install.zip

--- installCalc.sh
+++ installCalc.sh
@@ -4,6 +4,7 @@ cd /mnt/us/documents || exit 1
 # Check if GitHub is reachable
 if ! curl -s --head --request GET https://github.com/ | grep "200 OK" > /dev/null; then
     echo "Cannot reach GitHub. Check your internet connection."
+    sleep 2
     exit 1
 fi
 
@@ -22,6 +23,6 @@ mv TMPR24jt2kh/*/basicCalculator/ /mnt/us/extensions/
 # Clean up
 rm -rf /mnt/us/TMPR24jt2kh/
 rm /mnt/us/documents/installCalc.sh
-
+rm /mnt/us/documents/install.zip
 echo "Installation complete!"
-
+sleep 2
